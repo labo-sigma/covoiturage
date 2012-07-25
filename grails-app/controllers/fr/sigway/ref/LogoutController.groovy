3241e6bb277a2dccd3c12f2fda231333969d541e
Teddy Poingt <zfleet@gmail.com>
2012-07-25 11:35:20 +0200

    Ajout plugin codenarc + correction code

--- grails-app/controllers/fr/sigway/ref/LogoutController.groovy
+++ grails-app/controllers/fr/sigway/ref/LogoutController.groovy
@@ -1,3 +1,4 @@
+package fr.sigway.ref
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class LogoutController {
---
+++ test/unit/fr/sigway/technique/DataHelperServiceTests.groovy
@@ -0,0 +1,17 @@
+package fr.sigway.technique
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(DataHelperService)
+class DataHelperServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
